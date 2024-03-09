06129f5913a8467f8279e40a931f025f134c7ba8
margin <mr353045@dal.ca>
2024-03-09 01:45:56 -0400

    allowed host updated!

--- qconnect_api/qconnect_api/settings.py
+++ qconnect_api/qconnect_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)yc5qmaxr&$8-v(3*!yyxs*7^hr&n9wxejb-aa)!ydn8g2t)_@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['qconnect-gitlab.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
