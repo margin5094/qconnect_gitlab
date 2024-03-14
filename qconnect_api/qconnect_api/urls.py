4cb7083cba21329dd4a7acece9fecf8b72f7eb9a
margin <mr353045@dal.ca>
2024-03-13 23:40:14 -0300

    temporary api deleted!

--- qconnect_api/qconnect_api/urls.py
+++ qconnect_api/qconnect_api/urls.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('aapi/', include('gitlabapi.urls')),
     path('api/', include('mongoAPI.urls')), 
 ]
